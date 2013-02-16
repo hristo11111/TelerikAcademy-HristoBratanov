01e08d6a15a6907db3f830fc8d0ce25b01030988
Hristo Bratanov <hristo_brat@yahoo.com>
2013-02-16 19:24:03 +0200

    Format

---
+++ testRepo2/DeclareIntAndAsignHEX.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class DeclareIntAndAsignHEX
+{
+    static void Main()
+    {
+        int i;
+        i = 0xFE;
+        Console.WriteLine(i);
+    }
+}
