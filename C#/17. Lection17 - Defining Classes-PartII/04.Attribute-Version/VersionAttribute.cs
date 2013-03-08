e2f1f54de52147357abe746f733c034d5e47ece8
Hristo Bratanov <hristo_brat@yahoo.com>
2013-03-08 22:56:57 +0200

    homework c# added lection17

---
+++ C#/17. Lection17 - Defining Classes-PartII/04.Attribute-Version/VersionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface | 
+    AttributeTargets.Enum | AttributeTargets.Method)]
+public class Version : Attribute
+{
+    public string Ver { get; private set; }
+
+    public Version(string ver)
+    {
+        this.Ver = ver;
+    }
+    
+}
