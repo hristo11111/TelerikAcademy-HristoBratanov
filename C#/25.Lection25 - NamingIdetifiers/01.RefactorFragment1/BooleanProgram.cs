51cc88749bc3824b6ac20675ed5c604f2ae9433c
Hristo Bratanov <hristo_brat@yahoo.com>
2013-04-16 09:24:44 +0300

    new HW

---
+++ C#/25.Lection25 - NamingIdetifiers/01.RefactorFragment1/BooleanProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class BooleanProgram
+{
+    class BooleanClass
+    {
+        public void ToString(bool value)
+        {
+            string valueToString = value.ToString();
+            Console.WriteLine(valueToString);
+        }
+    }
+
+    public static void Main()
+    {
+        BooleanClass boolClassObject = new BooleanClass();
+        boolClassObject.ToString(true);
+    }
+}
+
---
+++ C#/25.Lection25 - NamingIdetifiers/02.RefactorFragment2/HumanClassTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+class HumanClassTest
+{
+    enum Gender
+    {
+        Male,
+        Female
+    }
+
+    class Human
+    {
+        public Gender Gender { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    // ssn = Social Security Number
+    public void CreateHuman(int ssn)
+    {
+        Human newHuman = new Human();
+        newHuman.Age = ssn;
+        if (ssn % 2 == 0)
+        {
+            newHuman.Name = "BigBrother";
+            newHuman.Gender = Gender.Male;
+        }
+        else
+        {
+            newHuman.Name = "HotChick";
+            newHuman.Gender = Gender.Female;
+        }
+    }
+
+    static void Main()
+    {
+
+    }
+}
+
