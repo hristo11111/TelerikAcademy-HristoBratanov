b63448dc707e08ef3a7825d6d9dc4587ba321540
Hristo Bratanov <hristo_brat@yahoo.com>
2013-05-09 12:50:54 +0300

    after intall

---
+++ High-Quality-Code/32. Lection32 - Code-Tuning-and-Optimization/ComparePerformance/ComparePerformance1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+class ComparePerformance1
+{
+    static void Main()
+    {
+        //performance of add
+        Console.WriteLine("Performance of add:");
+        //int
+        int intNum = 1;
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+        intNum = intNum + 2;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(int)");
+
+        //long
+        long longNum = 1;
+        sw.Start();
+
+        longNum = longNum + 2;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(long)");
+
+        //float
+        float floatNum = 1f;
+        sw.Start();
+
+        floatNum = floatNum + 2;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(float)");
+
+        //double
+        double doubleNum = 1;
+        sw.Start();
+
+        doubleNum = doubleNum + 2;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(double)");
+
+        //decimal
+        decimal decimalNum = 1m;
+        sw.Start();
+
+        decimalNum = decimalNum + 2;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(decimal)");
+        Console.WriteLine();
+
+        //performance of substract
+        Console.WriteLine("Performance of substract:");
+        //int
+        intNum = 1;
+        sw.Start();
+
+        intNum = intNum - 2;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(int)");
+
+        //long
+        longNum = 1;
+        sw.Start();
+
+        longNum = longNum - 2;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(long)");
+
+        //float
+        floatNum = 1;
+        sw.Start();
+
+        floatNum = floatNum - 2;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(float)");
+
+        //double
+        doubleNum = 1;
+        sw.Start();
+
+        doubleNum = doubleNum - 2;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(double)");
+
+        //decimal
+        decimalNum = 1m;
+        sw.Start();
+
+        decimalNum = decimalNum - 2;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(decimal)");
+        Console.WriteLine();
+
+        //performance of increment
+        Console.WriteLine("Performance of increment:");
+        //int
+        intNum = 1;
+        sw.Start();
+
+        intNum++;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(int)");
+
+        //long
+        longNum = 1;
+        sw.Start();
+
+        longNum++;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(long)");
+
+        //float
+        floatNum = 1;
+        sw.Start();
+
+        floatNum++;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(float)");
+
+        //double
+        doubleNum = 1;
+        sw.Start();
+
+        doubleNum++;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(double)");
+
+        //decimal
+        decimalNum = 1m;
+        sw.Start();
+
+        decimalNum++;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(decimal)");
+        Console.WriteLine();
+
+        //performance of multiply
+        Console.WriteLine("Performance of multiply:");
+        //int
+        intNum = 1;
+        sw.Start();
+
+        intNum = intNum * intNum;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(int)");
+
+        //long
+        longNum = 1;
+        sw.Start();
+
+        longNum = longNum * longNum;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(long)");
+
+        //float
+        floatNum = 1;
+        sw.Start();
+
+        floatNum = floatNum * floatNum;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(float)");
+
+        //double
+        doubleNum = 1;
+        sw.Start();
+
+        doubleNum = doubleNum * doubleNum;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(double)");
+
+        //decimal
+        decimalNum = 1m;
+        sw.Start();
+
+        decimalNum = decimalNum * decimalNum;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(decimal)");
+        Console.WriteLine();
+
+        //performance of divide
+        Console.WriteLine("Performance of divide:");
+        //int
+        intNum = 1;
+        sw.Start();
+
+        intNum = intNum / intNum;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(int)");
+
+        //long
+        longNum = 1;
+        sw.Start();
+
+        longNum = longNum / longNum;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(long)");
+
+        //float
+        floatNum = 1;
+        sw.Start();
+
+        floatNum = floatNum / floatNum;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(float)");
+
+        //double
+        doubleNum = 1;
+        sw.Start();
+
+        doubleNum = doubleNum / doubleNum; ;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(double)");
+
+        //decimal
+        decimalNum = 1m;
+        sw.Start();
+
+        decimalNum = decimalNum / decimalNum;
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(decimal)");
+
+    }
+}
---
+++ High-Quality-Code/32. Lection32 - Code-Tuning-and-Optimization/ComparePerformance2/ComparePerformance2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+class ComparePerformance2
+{
+    static void Main()
+    {
+        double result;
+        //performance of square root
+        Console.WriteLine("Performance of square root:");
+        //float
+        float floatNum = 1f;
+        
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+        result = Math.Sqrt(floatNum);
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(float)");
+
+        //double
+        double doubleNum = 1;
+        sw.Start();
+
+        result = Math.Sqrt(doubleNum);
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(double)");
+
+        //decimal
+        decimal decimalNum = 1m;
+        sw.Start();
+
+        result = Math.Sqrt((double)decimalNum);
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(decimal)");
+        Console.WriteLine();
+
+        //performance of square root
+        Console.WriteLine("Performance of natural logarithm:");
+        //float
+        floatNum = 1f;
+
+        sw.Start();
+
+        result = Math.Log(floatNum);
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(float)");
+
+        //double
+        doubleNum = 1;
+        sw.Start();
+
+        result = Math.Log(doubleNum);
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(double)");
+
+        //decimal
+        decimalNum = 1m;
+        sw.Start();
+
+        result = Math.Log((double)decimalNum);
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(decimal)");
+        Console.WriteLine();
+
+        //performance of sin
+        Console.WriteLine("Performance of square root:");
+        //float
+        floatNum = 1f;
+
+        sw.Start();
+
+        result = Math.Sin(floatNum);
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(float)");
+
+        //double
+        doubleNum = 1;
+        sw.Start();
+
+        result = Math.Sin(doubleNum);
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(double)");
+
+        //decimal
+        decimalNum = 1m;
+        sw.Start();
+
+        result = Math.Sin((double)decimalNum);
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed + "(decimal)");
+    }
+}
