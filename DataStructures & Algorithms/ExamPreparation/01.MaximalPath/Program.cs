5bbbeb5c9d84a46db7a194522e2c46cdbcab3a3d
Hristo Bratanov <hristo_brat@yahoo.com>
2013-06-06 10:36:17 +0300

    new hw

--- DataStructures & Algorithms/ExamPreparation/01.MaximalPath/Program.cs
+++ DataStructures & Algorithms/ExamPreparation/01.MaximalPath/Program.cs
@@ -1,15 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _01.MaximalPath
+class Program
 {
-    class Program
+    static long maxSum;
+    static HashSet<Node> visited = new HashSet<Node>();
+
+    static void Main()
     {
-        static void Main(string[] args)
+        int N = int.Parse(Console.ReadLine());
+        Dictionary<long, Node> nodes = new Dictionary<long, Node>();
+
+        for (int i = 0; i < N - 1; i++)
         {
+            string input = Console.ReadLine();
+            string[] nums = input.Split(new char[] { '(', ')', '-', '<' }, StringSplitOptions.RemoveEmptyEntries);
+
+            long parent = long.Parse(nums[0]);
+            long child = long.Parse(nums[1]);
+
+            Node parentNode;
+
+            if (nodes.ContainsKey(parent))
+            {
+                parentNode = nodes[parent];
+            }
+            else
+            {
+                parentNode = new Node(parent);
+                nodes.Add(parent, parentNode);
+            }
+
+            Node childNode;
+            if (nodes.ContainsKey(child))
+            {
+                childNode = nodes[child];
+            }
+            else
+            {
+                childNode = new Node(child);
+                nodes.Add(child, childNode);
+            }
+
+            parentNode.AddChild(childNode);
+            childNode.AddChild(parentNode);
         }
+
+        foreach (var pair in nodes)
+        {
+            if (pair.Value.ChildrenCount == 1)
+            {
+                visited.Clear();
+                DFS(pair.Value, 0);
+            }
+        }
+
+        Console.WriteLine(maxSum);
+    }
+
+    static void DFS(Node node, long currentSum)
+    {
+        currentSum = currentSum + node.Value;
+        visited.Add(node);
+
+        for (int i = 0; i < node.ChildrenCount; i++)
+        {
+            if (visited.Contains(node.Children[i]))
+            {
+                continue;
+            }
+            
+            DFS(node.Children[i], currentSum);
+        }
+
+        if (node.ChildrenCount == 1 && currentSum > maxSum)
+        {
+            maxSum = currentSum;
+        }
+    }
+}
+
+public class Node
+{
+    private long value;
+    private List<Node> children;
+
+    public Node(long value)
+    {
+        this.value = value;
+        this.children = new List<Node>();
+    }
+
+    public void AddChild(Node child)
+    {
+        this.children.Add(child);
+    }
+
+    public long Value
+    {
+        get { return this.value; }
+    }
+
+    public List<Node> Children
+    {
+        get { return this.children; }
+    }
+
+    public int ChildrenCount
+    {
+        get { return this.children.Count; }
     }
 }
---
+++ DataStructures & Algorithms/ExamPreparation/02.EmployeesSalaries/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.EmployeesSalaries
+{
+    class Program
+    {
+        static int salaries = 0;
+
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+
+            string bigBossName = Console.ReadLine();
+            Employee bigBoss = new Employee(bigBossName);
+
+            Dictionary<string, Employee> employees = new Dictionary<string,Employee>();
+            employees.Add(bigBossName, bigBoss);
+
+            for (int i = 1; i < N; i++)
+            {
+                string name = Console.ReadLine();
+                Employee employee = new Employee(name);
+                employees.Add(name, employee);
+            }
+
+            for (int i = 0; i < M; i++)
+            {
+                string[] names = Console.ReadLine().Split(' ');
+                string superiorName = names[0];
+
+                for (int j = 1; j < names.Length; j++)
+                {
+                    employees[superiorName].AddSubordinate(employees[names[j]]);
+                }
+            }
+
+            DFS(employees[bigBossName]);
+            Console.WriteLine(salaries);
+        }
+
+        private static void DFS(Employee employee)
+        {
+            if (employee.Subordinates.Count == 0)
+            {
+                employee.Salary = 1;
+            }
+            
+
+            foreach (var subordinate in employee.Subordinates)
+            {
+                DFS(subordinate);
+                employee.Salary += subordinate.Salary;
+            }
+            salaries += employee.Salary;
+        }
+    }
+
+    class Employee
+    {
+        public string Name { get; private set; }
+        public int Salary { get; set; }
+        public List<Employee> Subordinates { get; private set; }
+
+        public Employee(string name)
+        {
+            this.Name = name;
+            this.Salary = 0;
+            this.Subordinates = new List<Employee>();
+        }
+
+        public void AddSubordinate(Employee employee)
+        {
+            this.Subordinates.Add(employee);
+        }
+    }
+}
